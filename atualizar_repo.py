0ce5caae63bbd67f0691fcf88d7296a08ba58914
elvis <elvis.desouza99@gmail.com>
2018-11-16 11:25:53 -0200

    atualizar_repo rm

--- atualizar_repo.py
+++ atualizar_repo.py
@@ -15,6 +15,11 @@ def atualizar():
 			Git().init()
 			Git().remote('add','origin','https://github.com/alvelvis/ACDC-UD.git')
 			Git().fetch()
+			if os.path.isfile('atualizar_repo.py'): os.remove('atualizar_repo.py')
+			if os.path.isfile('README.md'): os.remove('README.md')
+			if os.path.isfile('acdc_procura.py'): os.remove('acdc_procura.py')
+			if os.path.isfile('comparar_UD.py'): os.remove('comparar_UD.py')
+			if os.path.isfile('limpar_conllu.py'): os.remove('limpar_conllu.py')
 			Git().checkout('master')
 
 atualizar()
