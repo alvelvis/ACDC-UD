a2ed5e8703f98411d014425ac4f12afc2b287de5
elvis <elvis.desouza99@gmail.com>
2018-11-17 15:29:23 -0200

    tokenizar-conllu

--- atualizar_repo.py
+++ atualizar_repo.py
@@ -21,6 +21,7 @@ def atualizar():
 			if os.path.isfile('comparar_UD.py'): os.remove('comparar_UD.py')
 			if os.path.isfile('limpar_conllu.py'): os.remove('limpar_conllu.py')
 			if os.path.isfile('apenas_tokens.py'): os.remove('apenas_tokens.py')
+			if os.path.isfile('tokenizar_conllu.py'): os.remove('tokenizar_conllu.py')
 			Git().checkout('master')
 
 atualizar()
---
+++ tokenizar_conllu.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import re
+
+def limpar(texto):
+	novotexto = list()
+	for linha in texto.splitlines():
+
+		if len(linha.split('\t')) == 10:
+			novotexto.append(re.sub(r'^.*?\t(.*?)\t.*$', r'\1\t-\t-\t-\t-\t-\t-\t-\t-', linha))
+
+		elif linha.strip() == '': novotexto.append('')
+
+	return "\n".join(novotexto)
+
+def main(antes, depois):
+	if ':' in antes:
+		codificação = antes.split(':')[1]
+		antes = antes.split(':')[0]
+	else:
+		codificação = 'utf8'
+
+	if ':' in depois:
+		codificação2 = depois.split(':')[1]
+		depois = depois.split(':')[0]
+	else:
+		codificação2 = 'utf8'
+
+	antes = open(antes, 'r', encoding=codificação).read()
+	open(depois, 'w', encoding=codificação2).write(limpar(antes))
+
+if __name__ == "__main__":
+	if len(sys.argv) == 2 and sys.argv[1] == '--atualizar':
+		import atualizar_repo
+		atualizar_repo.atualizar()
+		print('Atualizado com sucesso!')
+		exit()
+	if len(sys.argv) < 3:
+		print("uso: tokenizar_conllu.py ud.conllu:utf8 tokenizado.conllu:utf8")
+	elif len(sys.argv) == 3:
+		main(sys.argv[1], sys.argv[2])
+		print('OK!')
+	else:
+		print("Argumentos excessivos")
