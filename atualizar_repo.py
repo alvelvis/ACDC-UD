24efe23aa62e964f1ee7098e1fa167d1e8938088
elvis <elvis.desouza99@gmail.com>
2018-11-17 19:43:50 -0200

    lista_arquivos

--- atualizar_repo.py
+++ atualizar_repo.py
@@ -1,3 +1,14 @@
+lista_arquivos = [
+    'atualizar_repo.py',
+    'README.md',
+    'acdc_procura.py',
+    'comparar_UD.py',
+    'limpar_conllu.py',
+    'apenas_tokens.py',
+    'tokenizar_conllu.py',
+    'udpipe_vertical.py',
+]
+
 def atualizar():
 	try:
 		from git import Git
@@ -13,14 +24,8 @@ def atualizar():
 		Git().init()
 		Git().remote('add','origin','https://github.com/alvelvis/ACDC-UD.git')
 		Git().fetch()
-		if os.path.isfile('atualizar_repo.py'): os.remove('atualizar_repo.py')
-		if os.path.isfile('README.md'): os.remove('README.md')
-		if os.path.isfile('acdc_procura.py'): os.remove('acdc_procura.py')
-		if os.path.isfile('comparar_UD.py'): os.remove('comparar_UD.py')
-		if os.path.isfile('limpar_conllu.py'): os.remove('limpar_conllu.py')
-		if os.path.isfile('apenas_tokens.py'): os.remove('apenas_tokens.py')
-		if os.path.isfile('tokenizar_conllu.py'): os.remove('tokenizar_conllu.py')
-		if os.path.isfile('udpipe_vertical.py'): os.remove('udpipe_vertical.py')
+		for arquivo in lista_arquivos:
+		    if os.path.isfile(arquivo): os.remove(arquivo)
 		Git().checkout('master')
 
 atualizar()
