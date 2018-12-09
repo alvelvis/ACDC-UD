46e536ca0a706a82096110d5d283ed6b93791d66
elvis <elvis.desouza99@gmail.com>
2018-12-09 11:09:47 -0200

    bugfix

--- generate_release.py
+++ generate_release.py
@@ -11,21 +11,18 @@ arquivo_conllu = arquivo_ids.split('.txt')[0] + '.conllu'
 
 ids = open(arquivo_ids, 'r').read()
 
+arquivos_conllu = list()
+for conllu in os.listdir(diretorio + 'documents'):
+	if os.path.isfile(diretorio + 'documents/' + conllu):
+		arquivos_conllu.extend(open(diretorio + 'documents/' + conllu, 'r').read().split('\n\n'))
+
 novo_conllu = list()
 for i, identificador in enumerate(ids.splitlines()):
 	if identificador.strip() != '':
-		for arquivo in os.listdir(diretorio + 'documents'):
-			achou = False
-			if os.path.isfile(diretorio + 'documents/' + arquivo):
-				conllu = open(diretorio + 'documents/' + arquivo, 'r').read().split('\n\n')
-				for sentence in conllu:
-					if '# sent_id = ' in sentence and sentence.split('# sent_id = ')[1].split('\n')[0] == identificador:
-						novo_conllu.append(sentence)
-						print(str(i) + '/' + len(ids.splitlines()) + ': ' + identificador)
-						achou = True
-					if achou:
-						break
-			if achou:
+		for sentence in arquivos_conllu:
+			if '# sent_id = ' in sentence and sentence.split('# sent_id = ')[1].split('\n')[0] == identificador:
+				novo_conllu.append(sentence)
+				print(str(i+1) + '/' + str(len(ids.splitlines())) + ': ' + identificador)
 				break
 
 open(arquivo_conllu, 'w').write("\n\n".join(novo_conllu) + '\n\n')
