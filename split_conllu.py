a52925150648ec0b4a998a9f7d10800295cf47fa
Elvis de Souza <elvis.desouza99@gmail.com>
2019-11-14 11:49:14 -0300

    fix split_conllu

--- split_conllu.py
+++ split_conllu.py
@@ -27,10 +27,10 @@ for conllu in conllus:
 	texto = open(pasta_conllu + '/' + conllu, 'r', encoding="utf8").read().split('\n\n')
 	for sentence in texto:
 		if '# sent_id = ' in sentence:
-			identificador = sentence.split('# sent_id = ')[1].split('-')[0]
+			identificador = sentence.split('# sent_id = ')[1].split("\n")[0].rsplit('-', 1)[0]
 			if not identificador in documents.keys():
 				documents[identificador] = dict()
-			documents[identificador][int(sentence.split('# sent_id = ')[1].split('-')[1].split()[0])] = sentence
+			documents[identificador][int(sentence.split('# sent_id = ')[1].split("\n")[0].rsplit('-', 1)[1])] = sentence
 
 documentos_organizados = dict()
 for documento in documents.keys():
@@ -39,15 +39,15 @@ for documento in documents.keys():
 	i = 0
 	while documents[documento]:
 		i += 1
-		if i in documents[documento] and documents[documento][i].split('# sent_id = ')[1].split('-')[1].split()[0] == str(i):
+		if i in documents[documento] and documents[documento][i].split('# sent_id = ')[1].split("\n")[0].rsplit('-', 1)[1] == str(i):
 			documentos_organizados[documento].append(documents[documento][i])
 			del documents[documento][i]
 			i = 0
 
-	nome = re.search(r'\d+', documento)[0]
-	for i in range(4 - len(nome)):
-		nome = '0' + nome
-	nome = re.search(r'\D+', documento)[0] + nome
+	#nome = re.search(r'\d+', documento)[0]
+	#for i in range(4 - len(nome)):
+		#nome = '0' + nome
+	#nome = re.search(r'\D+', documento)[0] + nome
 
-	print(nome)
-	open(pasta_conllu + '/documents/' + nome + '.conllu', 'w', encoding="utf8").write('\n\n'.join(documentos_organizados[documento]) + '\n\n')
+	print(documento)
+	open(pasta_conllu + '/documents/' + documento + '.conllu', 'w', encoding="utf8").write('\n\n'.join(documentos_organizados[documento]) + '\n\n')
