1de67c83b3eb58d006b90c6ec35087bf6c08fef1
Elvis de Souza <elvis.desouza99@gmail.com>
2020-04-17 20:30:18 -0300

    update

--- from_release_to_workbench.py
+++ from_release_to_workbench.py
@@ -2,15 +2,31 @@ import sys
 sys.path.append("/home/elvis/ACDC-UD")
 import estrutura_ud
 
-corpus = estrutura_ud.Corpus(recursivo=True)
-corpus.load(sys.argv[1])
+if len(sys.argv < 3):
+    sys.stderr.write("usage: release.conllu workbench.conllu")
+    exit()
 
-changes_to_be_made = []
-for sentid, sentence in corpus.sentences.items():
+release = estrutura_ud.Corpus(recursivo=True)
+release.load(sys.argv[1])
+workbench = estrutura_ud.Corpus(recursivo=True)
+workbench.load(sys.argv[2])
+
+for sentid, sentence in workbench.sentences.items():
     for t, token in enumerate(sentence.tokens):
         if token.deprel == "appos:parataxis":
-            changes_to_be_made.append({'sentid': sentid, 't': t, 'coluna': 'deprel', 'valor': token.deprel})
+            release.sentences[sentid].tokens[t].deprel = "appos:parataxis"
         if token.deprel == "ccomp:parataxis":
-            changes_to_be_made.append({'sentid': sentid, 't': t, 'coluna': 'deprel', 'valor': token.deprel})
-        if token.deprel == "aux" and token.lemma not in ["ir", "estar",  "ter", "haver"]:
-            print(token.lemma + " " + token.head_token.lemma)
+            release.sentences[sentid].tokens[t].deprel = "appos:parataxis"
+        if token.deprel == "xcomp" and token.upos == "VERB" and token.head_token.upos == "VERB":
+            token.deprel = token.head_token.deprel
+            token.dephead = token.head_token.dephead
+            token.head_token.upos = "AUX"
+            token.head_token.deprel = "aux"
+            token.head_token.dephead = token.id
+            for _t, _token in enumerate(sentence.tokens):
+                if _token.upos == "SCONJ" and _token.deprel == "mark" and _token.dephead == token.id:
+                    _token.upos = "ADP"
+                    _token.deprel = "compound"
+                _token.dephead = token.head_token.id
+
+print(release.to_str())
