76c8bd10f4994003c4120da94460e1c3d5279d75
Elvis de Souza <elvis.desouza99@gmail.com>
2020-04-27 12:58:52 -0300

    update

--- stanza_annotate.py
+++ stanza_annotate.py
@@ -13,9 +13,9 @@ nlp = stanza.Pipeline('pt')
 arquivos = {}
 arquivos_cru = {}
 diretorio = sys.argv[1] if sys.argv[1][-1] != "/" else sys.argv[1].rsplit("/", 1)[0]
-sys.stderr.write("\n" + diretorio + "/" + diretorio.rsplit("/", 1)[1] + ".p")
+sys.stderr.write("\n" + diretorio + "/" + f'{diretorio.rsplit("/", 1)[1] if "/" in diretorio else diretorio}' + ".p")
 if os.path.isdir(diretorio):
-    if not os.path.isfile(diretorio + "/" + diretorio.rsplit("/", 1)[1] + ".p"):
+    if not os.path.isfile(diretorio + "/" + f'{diretorio.rsplit("/", 1)[1] if "/" in diretorio else diretorio}' + ".p"):
         for arquivo in os.listdir(diretorio):
             if arquivo.endswith(".txt"):
                 with open(diretorio + "/" + arquivo, encoding="utf-8") as f:
@@ -24,10 +24,10 @@ if os.path.isdir(diretorio):
                     arquivos[arquivo.rsplit(".txt")[0]] = nlp(text)
                 except:
                     sys.stderr.write('\nerro: ' + arquivo)
-        with open(diretorio + "/" + diretorio.rsplit("/", 1)[1] + ".p", "wb") as f:
+        with open(diretorio + "/" + f'{diretorio.rsplit("/", 1)[1] if "/" in diretorio else diretorio}' + ".p", "wb") as f:
             pickle.dump(arquivos, f)
     else:
-        with open(diretorio + "/" + diretorio.rsplit("/", 1)[1] + ".p", "rb") as f:
+        with open(diretorio + "/" + f'{diretorio.rsplit("/", 1)[1] if "/" in diretorio else diretorio}' + ".p", "rb") as f:
             arquivos = pickle.load(f)
     for arquivo in arquivos:
         arquivos[arquivo] = CoNLL.convert_dict(arquivos[arquivo].to_dict())
@@ -51,7 +51,7 @@ for arquivo in sorted(arquivos):
     conllus[arquivo] = []
     for s, sentence in enumerate(arquivos[arquivo]):
         text = " ".join([token[1] for token in sentence if not '-' in token[0]])
-        sent_id = arquivo + "-" + str(s+1)
+        sent_id = (arquivo + "-" + str(s+1)).rsplit("/", 1)[1] if '/' in arquivo else (arquivo + "-" + str(s+1))
         sentences.append("# sent_id = " + sent_id + "\n# text = " + text + "\n" + "\n".join(["\t".join(token) for token in sentence]))
         conllus[arquivo].append("# sent_id = " + sent_id + "\n# text = " + text + "\n" + "\n".join(["\t".join(token) for token in sentence]))
 
